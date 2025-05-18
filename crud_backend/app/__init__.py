03137fb0aae9bca7fd23213f05d161414367e9b2
JoaoPedrosz <joaosilvajs013@gmail.com>
2025-05-17 21:46:17 -0300

    2

--- crud_backend/app/__init__.py
+++ crud_backend/app/__init__.py
@@ -8,7 +8,7 @@ app.config.from_object(Config)
 
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-mongodb_client = PyMongo(app, uri="mongodb+srv://joaosilva:abc123456@com759.d8vet.mongodb.net/?retryWrites=true&w=majority&appName=COM759")
+mongodb_client = PyMongo(app, uri="mongodb+srv://joaosilva:abc123456@com759.d8vet.mongodb.net/trabalho_COM759?retryWrites=true&w=majority&appName=COM759")
 db = mongodb_client.db
 
 from app import routes
