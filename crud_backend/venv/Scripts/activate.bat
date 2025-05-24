c287e27253dfad9fb46b2cc21c561d9c3edddbdf
JoaoPedrosz <joaosilvajs013@gmail.com>
2025-05-24 20:38:40 -0300

    1

--- crud_backend/venv/Scripts/activate.bat
+++ crud_backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\joaos\OneDrive\Área de Trabalho\COM759 Aula 9 - Arquivos\crud_backend\venv
+set VIRTUAL_ENV=C:\Users\joaos\OneDrive\Documentos\Trabalho_COM759\crud_backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
